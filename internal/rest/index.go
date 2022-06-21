13259f1caf6f9e36069a2d0ea5b161ab4c054106
Roman Trapickin <8594293+rntrp@users.noreply.github.com>
2022-06-21 23:06:41 +0200

    better index.html: meta, favicon, ETag

--- internal/rest/index.go
+++ internal/rest/index.go
@@ -2,6 +2,8 @@ package rest
 
 import (
 	_ "embed"
+	"encoding/base64"
+	"hash/fnv"
 	"net/http"
 	"strconv"
 )
@@ -11,8 +13,25 @@ var html []byte
 
 var htmlContentLength = strconv.Itoa(len(html))
 
-func Index(w http.ResponseWriter, _ *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	w.Header().Set("Content-Length", htmlContentLength)
+var htmlETag = b64ETag(html)
+
+func b64ETag(b []byte) string {
+	h := fnv.New64a()
+	if _, err := h.Write(b); err != nil {
+		panic(err)
+	}
+	b64 := base64.URLEncoding.EncodeToString(h.Sum(nil))
+	return "\"" + b64 + "\""
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	wh := w.Header()
+	wh.Set("ETag", htmlETag)
+	if r.Header.Get("If-None-Match") == htmlETag {
+		w.WriteHeader(http.StatusNotModified)
+		return
+	}
+	wh.Set("Content-Type", "text/html; charset=utf-8")
+	wh.Set("Content-Length", htmlContentLength)
 	w.Write(html)
 }
