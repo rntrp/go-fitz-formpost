20ec92c569644af807cb086e8967662205cbcc54
Roman Trapickin <8594293+rntrp@users.noreply.github.com>
2022-06-21 23:04:56 +0200

    temporary workaround for gen2brain/go-fitz/issues/55

--- internal/fitzimg/convert.go
+++ internal/fitzimg/convert.go
@@ -3,6 +3,7 @@ package fitzimg
 import (
 	"errors"
 	"io"
+	"io/ioutil"
 
 	"github.com/gen2brain/go-fitz"
 	"github.com/rntrp/go-fitz-formpost/internal/config"
@@ -14,6 +15,7 @@ func IsErrorFormatIssue(err error) bool {
 }
 
 func NumPage(src []byte) (int, error) {
+	defer preventGC(src)
 	doc, err := fitz.NewFromMemory(src)
 	if err != nil {
 		return 0, err
@@ -23,6 +25,7 @@ func NumPage(src []byte) (int, error) {
 }
 
 func Convert(src []byte, dst io.Writer, params *Params) error {
+	defer preventGC(src)
 	switch config.GetProcessingMode() {
 	case config.Interleaved:
 		return interleave(src, dst, params)
@@ -30,5 +33,15 @@ func Convert(src []byte, dst io.Writer, params *Params) error {
 		return inMemory(src, dst, params)
 	default:
 		return serial(src, dst, params)
+	}
+}
+
+// Prevent premature GC of the underlying byte array.
+// Workaround for https://github.com/gen2brain/go-fitz/issues/55
+func preventGC(b []byte) {
+	// Try to avoid optimization by performing some bogus logic
+	if len(b) > 0 {
+		// Write the first byte to /dev/null
+		ioutil.Discard.Write(b[:1])
 	}
 }
